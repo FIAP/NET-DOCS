02854987458d57cd72d5fef9bbd09790bc16f1d8
Mestre do Rafa <tadriano.net@gmail.com>
2024-08-29 14:16:12 -0300

    subindo ajustes

--- dotnet-docs/Program.cs
+++ dotnet-docs/Program.cs
@@ -1,4 +1,6 @@
 using dotnet_docs.Infra;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,28 +9,62 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
+#region [Swagger]
+//builder.Services.AddSwaggerGen(c =>
+//{
+//    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+//    {
+//        Title = "Minha API",
+//        Version = "v1",
+//        Description = "Esta é uma API exemplo para demonstrar Swagger",
+//        Contact = new Microsoft.OpenApi.Models.OpenApiContact
+//        {
+//            Name = "Thiago S Adriano",
+//            Email = "tadriano@fiap.br.com",
+//            Url = new Uri("https://seusite.com")
+//        },
+//        License = new Microsoft.OpenApi.Models.OpenApiLicense
+//        {
+//            Name = "MIT",
+//            Url = new Uri("https://opensource.org/licenses/MIT")
+//        }
+//    });
+
+//    c.SchemaFilter<EnumSchemaFilter>();
+//});
+#endregion
+
+#region [Swagger 2]
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    c.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
     {
-        Title = "Minha API",
-        Version = "v1",
-        Description = "Esta é uma API exemplo para demonstrar Swagger",
-        Contact = new Microsoft.OpenApi.Models.OpenApiContact
+        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+        Description = "Por favor, insira 'Bearer' [espaço] e o token JWT",
+        Name = "Authorization",
+        Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey
+    });
+
+    c.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement {
         {
-            Name = "Thiago S Adriano",
-            Email = "tadriano@fiap.br.com",
-            Url = new Uri("https://seusite.com")
-        },
-        License = new Microsoft.OpenApi.Models.OpenApiLicense
-        {
-            Name = "MIT",
-            Url = new Uri("https://opensource.org/licenses/MIT")
+            new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+            {
+                Reference = new Microsoft.OpenApi.Models.OpenApiReference
+                {
+                    Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
         }
     });
 
-    c.SchemaFilter<EnumSchemaFilter>(); 
+    c.SchemaFilter<EnumSchemaFilter>(); // Manter o filtro existente
 });
+#endregion
+
+
 
 var app = builder.Build();
 
