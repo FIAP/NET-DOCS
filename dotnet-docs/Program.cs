e671d4fb342405f263a7867d2e0a6a6adf8bd3a4
Mestre do Rafa <tadriano.net@gmail.com>
2024-08-29 11:36:51 -0300

    subindo ajustes

--- dotnet-docs/Program.cs
+++ dotnet-docs/Program.cs
@@ -1,3 +1,5 @@
+using dotnet_docs.Infra;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +7,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(
+   c =>
+   {
+       c.SchemaFilter<EnumSchemaFilter>();
+   });
 
 var app = builder.Build();
 
