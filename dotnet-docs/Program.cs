07f7cfb1d6cbae44cd4fbd1bebdb78d1cd467a5f
Mestre do Rafa <tadriano.net@gmail.com>
2024-08-29 11:43:43 -0300

    add swagger

--- dotnet-docs/Program.cs
+++ dotnet-docs/Program.cs
@@ -7,11 +7,28 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(
-   c =>
-   {
-       c.SchemaFilter<EnumSchemaFilter>();
-   });
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Minha API",
+        Version = "v1",
+        Description = "Esta é uma API exemplo para demonstrar Swagger",
+        Contact = new Microsoft.OpenApi.Models.OpenApiContact
+        {
+            Name = "Thiago S Adriano",
+            Email = "tadriano@fiap.br.com",
+            Url = new Uri("https://seusite.com")
+        },
+        License = new Microsoft.OpenApi.Models.OpenApiLicense
+        {
+            Name = "MIT",
+            Url = new Uri("https://opensource.org/licenses/MIT")
+        }
+    });
+
+    c.SchemaFilter<EnumSchemaFilter>(); 
+});
 
 var app = builder.Build();
 
